463b61eb7616bf20b7292a64511cf31b14f6ec3c
Bia Miranda <Bia@Macs-MacBook-Pro.local>
2024-07-17 21:46:30 -0300

    add: create user route

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import fastify from "fastify";
 import cors from "@fastify/cors";
+import { createUser } from "./routes/create-user";
+import { validatorCompiler, serializerCompiler } from "fastify-type-provider-zod";
 
 const app = fastify()
 
 app.register(cors, {
     origin: '*',
 })
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(createUser)
 
 app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
     console.log("Servidor rodando!")
